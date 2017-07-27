ec116edd6a6c442978f82da6f04c60ca0d5fc634
Jan Szumiec <jan.szumiec@gmail.com>
2017-07-27 23:50:20 +0100

    Added BLE detection code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
 
-void setup() {
+#define LED 33
 
+bool detectBluetooth() {
+  int baudrate[] = {1200,2400,4800,9600,19200,38400,57600,115200};
+  for (int i = 0; i < 8; i++) {
+    Serial3.begin(baudrate[i]);
+    Serial3.write("AT");
+    delay(500);
+    if (Serial3.read() == 'O' && Serial3.read() == 'K') {
+      goto found;
+    }
+  }
+  return false;
+
+  found:
+  Serial3.write("AT+NAMELimiTTer");
+  delay(500);
+  Serial3.write("AT+RESET");
+  delay(500);
+  return true;
+}
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, detectBluetooth() ? 1 : 0);
 }
 
 void loop() {
-  
+
 }
